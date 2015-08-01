f5d28682490298d8924b502a7615d0b37fa4ab18
Rod <supgeek.rod@gmail.com>
2015-08-01 18:21:06 +0800

    modify side panel

--- resources/views/snippets/panel-side.blade.php
+++ resources/views/snippets/panel-side.blade.php
@@ -1,6 +1,7 @@
 <div class="panel panel-default">
-    <div class="panel-heading">{{ trans('app.3N1WebSite') }}</div>
+    <div class="panel-heading">GanPlus </div>
     <div class="panel-body">
-        {{ trans('app.Quickly build a bbs/blog/cms web site') }}
+        赣+ 是一个有情怀有追求的赣州青年社区 <br>
+        欢迎你加入我们，与我们一起交流、交流和交流
     </div>
 </div>
